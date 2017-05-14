d0ff51db6b6c587c05c8e0381f4fe99d3e225a9b
Allan Liebold <allan.liebold@gmail.com>
2017-05-13 18:07:41 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 function myGame(){
-
+  
   alert('Time for a guessing game!');
 
   var ready = prompt('Are you ready? Y/N').toLowerCase();
@@ -16,37 +16,51 @@ function myGame(){
     alert('Well, I\'m starting anyway.');
   }
 
-  var answer1 = prompt('Question 1: Are you bigger than a bread box? Y/N').toLowerCase();
-  console.log('Answer 1:', answer1);
+  alert('I bet I can guess what you are in five perfectly chosen questions.');
 
-  var answer2 = prompt('Question 2: Are you alive? Y/N').toLowerCase();
-  console.log('Answer 2:', answer2);
+  var qArr = ['Question 1: Are you bigger than a bread box? Y/N',
+              'Question 2: Are you alive? Y/N',
+              'Question 3: Do you mostly come out at night, mostly? Y/N',
+              'Question 4: Are you vulnerable to fire? Y/N',
+              'Question 5: Can you read minds? Y/N',
+              ];
 
-  var answer3 = prompt('Question 3: Do you mostly come out at night, mostly? Y/N').toLowerCase();
-  console.log('Answer 3:', answer3);
+  function askQuestion(qNumber) {
+    var currentAnswer = prompt(qArr[qNumber]).toLowerCase();
+    console.log('Answer for question ' + qNumber + ' is ' + currentAnswer);
+    return currentAnswer;
+  }
 
-  var answer4 = prompt('Question 4: Are you vulnerable to fire? Y/N').toLowerCase();
-  console.log('Answer 4:', answer4);
-
-  var answer5 = prompt('Question 5: Can you read minds? Y/N').toLowerCase();
-  console.log('Answer 5:', answer5);
+  for (var i = 0; i < qArr.length; i++){
+      var currentAnswer = askQuestion(i);
+      if (currentAnswer === 'yes' || currentAnswer === 'y'){
+        console.log('choice' + i + ' is true');
+        this['choice' + i] = true;
+      } else if (currentAnswer === 'no' || currentAnswer === 'n'){
+        console.log('choice' + i + ' is false');
+        this['choice' + i] = false;
+      } else {
+        alert('I said Y/N!');
+        i--;
+      }
+  }
 
   alert('Okay, I got this. Thinking... thinking...');
   alert('You are a...');
 
-  if(answer1 === 'n' || answer1 === 'no'){
+  if(!choice0){
     alert('Teeny tiny...');
   }
-  if(answer2 === 'n' || answer2 === 'no'){
+  if(!choice1){
     alert('Undead...');
   }
-  if(answer3 === 'y' || answer3 === 'yes'){
+  if(choice2){
     alert('Nocturnal...');
   }
-  if(answer4 === 'n' || answer4 === 'no'){
+  if(!choice3){
       alert('Fireproof...');
   }
-  if(answer5 === 'y' || answer5 === 'yes'){
+  if(choice4){
     alert('Telepathic...');
   }
 
@@ -64,17 +78,18 @@ function myGame(){
   var guess;
   var lowes = Math.floor(Math.random()*20) + 1;
   var tries = 4;
+
   console.log('Psst. The answer is...', lowes);
 
   while(tries > 0){
     console.log('Last guess:', guess);
     console.log('Tries left:', tries);
-
     if (tries > 1){
       guess = parseInt(prompt('How many Lowe\'s could Rob Lowe rob if Rob Lowe could rob Lowe\'s? You have ' + tries + ' tries left.'));
     } else {
       guess = parseInt(prompt('Last chance. How many?'));
     }
+
     if (guess < lowes){
       alert('You underestimate Mr. Lowe.');
       tries--;
@@ -91,45 +106,53 @@ function myGame(){
       break;
     }
   }
-  if (guess != lowes){
+
+  if (guess != lowes)
     alert('Bzzzt! Game over! Now you\'ll never know the exact extent of Robe Lowe\'s robbery prowess!');
-  }
+
   alert('Time to get serious now.');
   alert('As you may have guessed, I am an entity that exists beyond your limited comprehension.');
   alert('As such, in order to interact with your kind, I have manifested in no fewer than four of your so-called "united" states of being.');
-  alert('Besides Washington, name one of the three other states in which I have lived. No abbrevs please. And, yes, spelling counts. You get 6 chances to give a correct answer.');
+  alert('Besides Washington, name one of the three other states in which I have existed. No abbrevs please. And, yes, spelling counts. 3 wrong answers and it\'s game over. Get at least 1 right and you win! Got it? Okay...');
 
-  var flag;
+  var correct;
+  var doOver;
   var lived = ['california', 'georgia', 'illinois'];
-  var counter = 6;
+  var counter = 3;
   var points = 0;
 
   while(counter > 0){
-    console.log("Tries left:", counter);
-    var state = prompt('Where have I lived? You have ' + counter + ' guesses.').toLowerCase();
-  /* I want certain answers to make the user guess again without counting as a try.
-    if (state.length < 3 || state === 'washington'){
-      alert('No, no, no. Try that again.');
-    }
-  */
+    console.log('Tries left:', counter);
+    console.log('Points: ', points);
 
-  // flag needs to be reset to false at the start of each loop.
-    flag = false;
+// reset the correct and doOver flags at the start of each loop
+    correct = false;
+    doOver = false;
+
+    var state = prompt('Where have I lived? You have ' + counter + ' tries left.').toLowerCase();
+    if (state.length < 3 || state === 'washington'){
+      alert('No, no, no. Follow the rules!');
+      doOver = true;
+    }
+
     for(var i = 0; i < lived.length; i++){
       console.log('Current State:', lived[i]);
       if(state === lived[i]){
         alert('Correct!');
         lived.splice(i, 1);
-        flag = true;
+        correct = true;
         break;
       }
     }
-    if(flag){
+    if(doOver){
+      console.log('Do over!');
+    } else if(correct){
       points++;
     } else {
       alert('Nope!');
       counter--;
     }
+
     if(points === 3){
       break;
     }
